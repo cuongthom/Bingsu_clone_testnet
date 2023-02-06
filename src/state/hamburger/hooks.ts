ce99ce3f4c41a42a999a334a8c55e8b8454d3d64
cuong <huydz@gmail.com>
2023-02-06 10:24:17 +0700

    new

---
+++ src/state/hamburger/hooks.ts
@@ -0,0 +1,18 @@
+import {useDispatch, useSelector} from "react-redux";
+import {useCallback} from "react";
+import {AppState} from "../index";
+import {toggleOpen} from "./reducer";
+
+
+export const useHamburger = () => {
+  
+  const hamburgerMenu = useSelector((state: AppState) => state.hamburger)
+
+  const dispatch = useDispatch()
+  const onToggleHamburger = useCallback(() => dispatch(toggleOpen(undefined)), [dispatch])
+
+  return {
+    hamburgerMenu,
+    onToggleHamburger
+  }
+}
---
+++ src/state/modal/hooks.ts
@@ -0,0 +1,19 @@
+import {useDispatch, useSelector} from "react-redux";
+import {useCallback} from "react";
+import {AppState} from "../index";
+import {close, open} from "./reducer";
+
+
+export const useModal = () => {
+  const modal = useSelector((state: AppState) => state.modal)
+  const dispatch = useDispatch()
+  const onCloseModal = useCallback(() => dispatch(close(undefined)), [dispatch])
+  const onOpenModal = useCallback(() => dispatch(open(undefined)), [dispatch])
+console.log("modal",modal);
+
+  return {
+    modal,
+    onCloseModal,
+    onOpenModal
+  }
+}
